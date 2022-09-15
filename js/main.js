f9057963a3031e359dd6090011892cea9dd1b3f0
DangThaoTrang <thaotrangcongviec@gmail.com>
2022-09-15 18:41:43 +0700

    Cart

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
-let cartPhone = [];
+let cartPhone = new Cart();
+let arrListCart = [];
 function dom(element) {
-    return document.querySelector(element);
+  return document.querySelector(element);
 }
+// lấy dữ liệu từ axios
 getProducts();
 function getProducts(searchType) {
-    getApiProducts(searchType)
-        .then((respone) => {
-            let productList = respone.data.map((product) => {
-                return new Product(
-                    product.name,
-                    product.price,
-                    product.screen,
-                    product.backCamera,
-                    product.frontCamera,
-                    product.image,
-                    product.discription,
-                    product.type,
-                    product.id,
-                );
-            });
-            display(productList);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+  getFromLocalStorage();
+  getApiProducts(searchType)
+    .then((respone) => {
+      let productList = respone.data.map((product) => {
+        return new Product(
+          product.name,
+          product.price,
+          product.screen,
+          product.backCamera,
+          product.frontCamera,
+          product.image,
+          product.discription,
+          product.type,
+          product.id
+        );
+      });
+      display(productList);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+// hiển thị giao diện sản phẩm
 function display(products) {
-    let output = products.reduce((result, product) => {
-        return result + `
-        <div class="card">
+  let output = products.reduce((result, product) => {
+    return (
+      result +
+      `
+        <div class="card cart-item">
             <div class="card-header">
                 <img src="${product.image}" class="image">  
             </div>
@@ -41,79 +47,209 @@ function display(products) {
             </div>
             <div class="card-footer">
                 <div class="price">$${product.price}</div>
-                <button class="btn-add" data-id ="${product.id}">Add cart</button>
+                <button class="btn-add btn btn-success" data-id ="${product.id}">Add cart</button>
             </div>
         </div>
         `
-    }, "")
-    dom("#products").innerHTML = output;
+    );
+  }, "");
+  dom("#products").innerHTML = output;
 }
+
+// hiển thị giỏ hàng
 function displayListPhone(cartPhone) {
-    let output = cartPhone.reduce((result, product) => {
-        return result + `
+  if (cartPhone.cart.length === 0) {
+    dom(".text").style.display = "block";
+  } else {
+    dom(".text").style.display = "none";
+  }
+  let output = cartPhone.cart.reduce((result, product) => {
+    return (
+      result +
+      `
             <tr>
                 <td>
                     <img src="${product.image}" width="60px" height="60px">
                 </td>
                 <td class= "phoneName">${product.name}</td>
                 <td>
-                    <button onclick = "decrease()"><i class="fa-solid fa-angle-left"></i></button>
-                    <span >1</span>
-                    <button onclick = "increase()"><i class="fa-solid fa-angle-right"></i></button>
+                    <button onclick = "decrease('${product.id}')"><i class="fa-solid fa-angle-left"></i></button>
+                    <span>${product.quantity}</span>
+                    <button onclick = "increase('${product.id}')"><i class="fa-solid fa-angle-right"></i></button>
                 </td>
                 <td>$${product.price}</td>
                  <td>
-                    <button class ="btn-PhoneTrash" onclick ="delete()">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
+                 <button class ="btn-PhoneTrash btn-danger" >
+                 <i class="fa-solid fa-trash" data-id="${product.id}" data-type="delete"></i>
+             </button>
                 </td>
             </tr>
         `
-    }, "")
-    dom("#cart-item").innerHTML = output;
+    );
+  }, "");
+  dom("#cart-item").innerHTML = output;
+  totalCart();
 }
+// Lưu dữ liệu vào localStorange
+function saveLocalStorage(cartPhone) {
+  localStorage.setItem("cart", JSON.stringify(cartPhone.cart));
+}
+function getFromLocalStorage() {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  if (items) {
+    items.forEach((product) => {
+      const p = new Product(
+        product.name,
+        product.price,
+        product.screen,
+        product.backCamera,
+        product.frontCamera,
+        product.image,
+        product.discription,
+        product.type,
+        product.id,
+        product.quantity
+      );
+      cartPhone.addToCart(p);
+      countCartItem();
+      displayListPhone(cartPhone);
+    });
+  }
+}
+//=============================================================
+// 1. search
 function searchProduct() {
-    let items = dom("#chooseProducts").value
-    getProducts(items);
+  let items = dom("#chooseProducts").value;
+  getProducts(items);
 }
+// 2. đếm slg sản phẩm
+function countCartItem() {
+  dom("#number").innerHTML = cartPhone.count();
+}
+// 3. Thêm sp
 dom("#products").addEventListener("click", (e) => {
-    let idProduct = e.target.getAttribute("data-id");
-    if (!idProduct) {
-        return false;
-    }
-    getApiProductById(idProduct)
-        .then((respone) => {
-            let smartPhone = respone.data.map((product) => {
-                return new Product(
-                    product.name,
-                    product.price,
-                    product.screen,
-                    product.backCamera,
-                    product.frontCamera,
-                    product.image,
-                    product.discription,
-                    product.type,
-                    product.id,
-                );
-            });
-            // let ac = cartPhone.filter((phone) => phone.id === smartPhone[0].id)
-            // console.log(ac);
-            // for (let i = 0; i < cartPhone.length; i++) {
-            //     if (cartPhone[i].id === smartPhone[0].id) {
-            //         console.log("có");
-            //     }
-            // }
-            cartPhone.push(smartPhone[0]);
-            console.log(cartPhone);
-            if (cartPhone === []) {
-                dom(".text").style.display = "block"
-            } else {
-                dom(".text").style.display = "none"
-                dom("#number").innerHTML = cartPhone.length;
-                displayListPhone(cartPhone);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  let idProduct = e.target.getAttribute("data-id");
+  if (!idProduct) {
+    return false;
+  }
+  getApiProductById(idProduct)
+    .then((respone) => {
+      let smartPhone = respone.data.map((product) => {
+        return new Product(
+          product.name,
+          product.price,
+          product.screen,
+          product.backCamera,
+          product.frontCamera,
+          product.image,
+          product.discription,
+          product.type,
+          product.id,
+          1
+        );
+      });
+      cartPhone.addToCart(smartPhone[0]);
+      saveLocalStorage(cartPhone);
+
+      if (cartPhone.cart.length === 0) {
+        dom(".text").style.display = "block";
+      } else {
+        dom(".text").style.display = "none";
+        dom("#number").innerHTML = cartPhone.cart.reduce(
+          (total, item) => total + item.quantity,
+          0
+        );
+        displayListPhone(cartPhone);
+      }
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
 });
+
+// 4. xoá sản phẩm
+dom("#modal-close").addEventListener("click", (e) => {
+  let id = e.target.getAttribute("data-id");
+  let typeEl = e.target.getAttribute("data-type");
+  if (typeEl === "delete") {
+    deleteProduct(id);
+  }
+});
+function deleteProduct(productId) {
+  cartPhone.cart = cartPhone.cart.filter((phone) => phone.id !== productId);
+  if (cartPhone.cart.length === 0) {
+    dom(".text").style.display = "block";
+  } else {
+    dom(".text").style.display = "none";
+  }
+  countCartItem();
+  saveLocalStorage(cartPhone);
+  displayListPhone(cartPhone);
+}
+
+// 5. tính tổng tiền giỏ hàng
+function totalCart() {
+  let totalPrice = cartPhone.cart.reduce((total, item) => {
+    return total + item.quantity * item.price;
+  }, 0);
+  dom("#money").innerHTML = totalPrice;
+}
+
+// 6. tăng giảm số lượng giỏ hàng
+//  + giảm
+function decrease(productId) {
+  cartPhone.decreaseItemFromCart(productId);
+  saveLocalStorage(cartPhone);
+  displayListPhone(cartPhone);
+  countCartItem();
+}
+//  + tăng
+function increase(productId) {
+  cartPhone.increaseItemFromCart(productId);
+  saveLocalStorage(cartPhone);
+  displayListPhone(cartPhone);
+  countCartItem();
+}
+//=============================================================
+// A. Clean Cart
+dom("#btn-garbage").addEventListener("click", () => {
+  cartPhone.cart.length = 0;
+  displayListPhone(cartPhone);
+  saveLocalStorage(cartPhone);
+});
+
+dom("#btn-purchase").addEventListener("click", () => {
+  if ((cartPhone.cart.length = 0)) {
+    return false;
+  } else {
+    pay();
+  }
+});
+function pay() {
+  let toPay = document.getElementsByClassName("#money").innerText;
+  let productNames = cartPhone.cart.map((item) => {
+    return `<span>${item.quantity} x ${item.name}</span>`;
+  });
+  let productPrice = cartPhone.cart.map((item) => {
+    return `<span>${item.quantity * item.price}</span>`;
+  });
+  return `
+  <div class='invoice'>
+    <div class='shipping-items'>
+      <div class='item-names'>${productNames.join("")}</div>
+      <div class='items-price'>${productPrice.join("+")}</div>
+    </div>
+  <hr>
+    <div class='payment'>
+      <em>payment</em>
+      <div>
+        <p>total amount to be paid:</p><span class='pay'>₹ ${toPay}</span>
+      </div>
+    </div>
+    <div class='order'>
+      <button onclick='order()' class='btn-order btn'>Order Now</button>
+      <button onclick='buy(0)' class='btn-cancel btn'>Cancel</button>
+    </div>
+  </div>`;
+}
--- js/product.js
+++ js/product.js
@@ -1,11 +1,23 @@
-function Product(name, price, screen, backCamera, frontCamera, image, description, type, id) {
-    this.name = name;
-    this.price = price;
-    this.screen = screen;
-    this.backCamera = backCamera;
-    this.frontCamera = frontCamera;
-    this.image = image;
-    this.description = description;
-    this.type = type;
-    this.id = id;
+function Product(
+  name,
+  price,
+  screen,
+  backCamera,
+  frontCamera,
+  image,
+  description,
+  type,
+  id,
+  quantity
+) {
+  this.name = name;
+  this.price = price;
+  this.screen = screen;
+  this.backCamera = backCamera;
+  this.frontCamera = frontCamera;
+  this.image = image;
+  this.description = description;
+  this.type = type;
+  this.id = id;
+  this.quantity = quantity;
 }
